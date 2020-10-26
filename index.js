a76b53115fc852d4387f64a106b6e33bd4669880
Colin Meerveld <colin.meerveld@gmail.com>
2020-10-26 21:58:50 +0000

    inital commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// index.js
+
+const serverless = require('serverless-http');
+const express = require('express');
+const graphy = require('graphy');
+const trig_read = graphy.content.trig.read;
+const xml_scribe = graphy.content.xml.scribe;
+const dataset = graphy.memory.dataset.fast;
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/trig2xml', (ds_req, ds_res) => {
+    var data = "";
+    (async() => {
+    let xml = await ds_req.pipe(trig_read())
+        .pipe(xml_scribe())
+            .on("data", (xml_data) => {
+                data += xml_data;
+            })
+        .until('finish', true);
+    ds_res.status(200).end(data);
+})();
+
+    
+});
+
+module.exports.handler = serverless(app);
